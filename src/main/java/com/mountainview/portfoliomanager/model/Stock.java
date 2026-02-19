ff82a9d0eebbc085b5a991ea40ebb124d041a547
Kenneth Dorji <kennethdorji@gmail.com>
2026-02-19 14:28:05 -0500

    Create stock class to put stock inside user portfolio

--- src/main/java/com/mountainview/portfoliomanager/model/Stock.java
+++ src/main/java/com/mountainview/portfoliomanager/model/Stock.java
@@ -1,4 +1,18 @@
 package com.mountainview.portfoliomanager.model;
 
-public class Stock {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class Stock extends EquitySecurity {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Stock.class);
+
+    public Stock(TransactionType transactionType, String instrumentSymbol, BigDecimal quantity, BigDecimal purchasePrice) {
+        super(transactionType, instrumentSymbol, quantity, purchasePrice);
+    }
+
 }
