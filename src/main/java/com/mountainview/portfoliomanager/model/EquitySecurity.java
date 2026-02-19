71190b459530dcfc2c30968a95a36faff355d6c1
Kenneth Dorji <kennethdorji@gmail.com>
2026-02-19 14:04:08 -0500

    Remove unused import statement.

--- src/main/java/com/mountainview/portfoliomanager/model/EquitySecurity.java
+++ src/main/java/com/mountainview/portfoliomanager/model/EquitySecurity.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.time.LocalDate;
 import java.util.*;
 
 public abstract class EquitySecurity {
