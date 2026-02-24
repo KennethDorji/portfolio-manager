ea834cddba8d2bd73a558dc00ae9fb105a4fa86f
Kenneth Dorji <kennethdorji@gmail.com>
2026-02-23 19:05:24 -0500

    Add logger

--- src/main/java/com/mountainview/portfoliomanager/service/PortfolioManager.java
+++ src/main/java/com/mountainview/portfoliomanager/service/PortfolioManager.java
@@ -2,8 +2,12 @@ package com.mountainview.portfoliomanager.service;
 
 import com.mountainview.portfoliomanager.model.Portfolio;
 import com.mountainview.portfoliomanager.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PortfolioManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PortfolioManager.class);
 
     public Portfolio createPortfolio(User user, String portfolioName) {
         return new Portfolio(user.getUserId(), portfolioName);
